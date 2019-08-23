4d284b35534cb61c28708b0ccdc9f93d8d90cb98
fullsnackdeveloper <thefullsnackdeveloper@gmail.com>
2019-08-23 12:42:23 -0400

    initial commit

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,6 +4,13 @@ import classNames from "classnames";
 import "./Button.sass";
 
 export default class Button extends Component {
+  static deafultProps = {
+    type: "primary",
+    size: "regular",
+    icon: null,
+    onColor: false
+  };
+
   render() {
     const { children, type, icon, onColor, size } = this.props;
     let classes = classNames("Button", type, size, {
